4892b587d9dfc0c063c2e37ba7f967e309d65c50
Vasanth <vasanthps@live.com>
2016-08-21 14:56:13 +0530

    fixed db config and bower comps

--- www/js/db-config.js
+++ www/js/db-config.js
@@ -1,20 +1,13 @@
 angular.module('starter')
-.constant(
-'json1', 
-{  
-    "db":"remgoDB",
-    "collection": "Reminder" ,
-       "documents" :
-        [  
-            {
-                "name": "id",
-                "label": "",
-                "location": null,
-                "repeat": false,
-                "maxRepeatPerDay": 1,
-                "timeRange": null
-
-            }
-        ]
-}
-)
+.constant('json1', {  
+    "db" : "remgoDB",
+    "collection" : "Reminder",
+    "documents" : [{
+        "id": "12345",
+        "label": "",
+        "location": null,
+        "repeat": false,
+        "maxRepeatPerDay": 1,
+        "timeRange": null
+    }]
+});
